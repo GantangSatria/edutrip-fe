3883f582c976228ad35c3208e65d8dbe6e3c3771
GantangSatria <gantang610@gmail.com>
2026-05-17 11:28:45 +0700

    feat: add services for authentication, hotels, tourism, halal restaurants, souvenir shops, worship facilities, and transportation

---
+++ types/planData.ts
@@ -0,0 +1,13 @@
+import { FasilitasIbadah } from "./fasilitasIbadah";
+import { Hotel } from "./hotel";
+import { RestoranHalal } from "./restoranHalal";
+import { TokoOlehOleh } from "./tokoOlehOleh";
+import { Wisata } from "./wisata";
+
+export type PlanData = {
+  wisata: Wisata[];
+  hotel: Hotel[];
+  restoran_halal: RestoranHalal[];
+  fasilitas_ibadah: FasilitasIbadah[];
+  toko_oleh_oleh: TokoOlehOleh[];
+};
